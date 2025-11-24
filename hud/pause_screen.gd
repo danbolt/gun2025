fe047dc60939d2d52312f557fb02511a8f4c513e
Daniel Savage <dansvg@gmail.com>
2025-11-24 10:52:33 +0900

    Pause screen

--- hud/pause_screen.gd
+++ hud/pause_screen.gd
@@ -1,7 +1,23 @@
 extends Control
 
+@onready var resume_button := %ResumeButton
+@onready var exit_game_button := %ExitGameButton
+
+func on_visibility_changed() -> void:
+	if visible:
+		resume_button.grab_focus()
+
 func _ready() -> void:
 	process_mode = Node.PROCESS_MODE_ALWAYS
+	
+	exit_game_button.focus_neighbor_bottom = resume_button.get_path()
+	resume_button.focus_neighbor_top = exit_game_button.get_path()
+	
+	resume_button.pressed.connect(func () -> void: get_tree().paused = !get_tree().paused)
+	exit_game_button.pressed.connect(func () -> void: get_tree().quit())
+	
+	visibility_changed.connect(on_visibility_changed)
+	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
