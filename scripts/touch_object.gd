d3a1341c5feabc41d05d820d73ce20ead9c43a30
Daniel Savage <dansvg@gmail.com>
2025-11-25 20:17:18 +0900

    Map data

--- scripts/touch_object.gd
+++ scripts/touch_object.gd
@@ -1,9 +1,21 @@
+@tool 
 class_name TouchObject extends CharacterBody3D
 
-@export_flags("One", "Two", "Three", "Four") var numbers: int = 0
+@export var a: bool = false
+@export var b: bool = false
+@export var c: bool = false
+@export var d: bool = false
+
+func _func_godot_apply_properties(properties: Dictionary) -> void:
+	a = int(properties.get("1", 0))
+	b = int(properties.get("2", 0))
+	c = int(properties.get("3", 0))
+	d = int(properties.get("4", 0))
 
 func _physics_process(delta: float) -> void:
-	print(delta)
+	if Engine.is_editor_hint():
+		return
+	
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
