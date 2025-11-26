95715242911e59c38addd0893bfdebad989ff4e7
Daniel Savage <dansvg@gmail.com>
2025-11-26 19:54:46 +0900

    Flags

--- scripts/touch_object.gd
+++ scripts/touch_object.gd
@@ -1,16 +1,12 @@
 @tool 
 class_name TouchObject extends CharacterBody3D
 
-@export var a: bool = false
-@export var b: bool = false
-@export var c: bool = false
-@export var d: bool = false
+@export_flags_2d_physics var spawn_flags: int = 0
+@export_flags_2d_physics var mask_flags: int = 15
 
 func _func_godot_apply_properties(properties: Dictionary) -> void:
-	a = int(properties.get("1", 0))
-	b = int(properties.get("2", 0))
-	c = int(properties.get("3", 0))
-	d = int(properties.get("4", 0))
+	spawn_flags = int(properties.get("spawnflags", 0))
+	mask_flags = int(properties.get("maskflags", 15))
 
 func _physics_process(delta: float) -> void:
 	if Engine.is_editor_hint():
