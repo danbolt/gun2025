e760370dcf98e4228206be719f66f67550974ed3
Daniel Savage <dansvg@gmail.com>
2025-12-03 12:08:56 +0900

    Remove unused stuff

--- automation/linux/get_blender.sh
+++ automation/linux/get_blender.sh
@@ -1,15 +1,20 @@
 # Gets Blender from its website
 
-blender_tar_name='blender-4.5.1-linux-x64.tar.xz'
-blender_tar_url='https://ftp.halifax.rwth-aachen.de/blender/release/Blender4.5/blender-4.5.1-linux-x64.tar.xz'
+blender_tar_name='blender-5.0.0-linux-x64.tar.xz'
+blender_tar_url='https://ftp.halifax.rwth-aachen.de/blender/release/Blender5.0/blender-5.0.0-linux-x64.tar.xz'
 
 blender_unzipped_dir="blender/blender-5.0.0-linux-x64"
 
-godot_binary_name='Godot_v4.5-stable_linux.x86_64'
+godot_binary_name='Godot_v4.5.1-stable_linux.x86_64'
 
 # Install prerequisite xorg stuff
 #sudo apt update --assume-yes && sudo apt install xorg blender xz-utils xvfb --assume-yes
 
+if [ ! -e automation/linux/$blender_tar_name ]; then
+    curl -L -o automation/linux/$blender_tar_name $blender_tar_url
+else
+	echo "Blender already downloaded"
+fi
 
 if [ ! -e $blender_unzipped_dir ]; then
 	tar xf automation/linux/$blender_tar_name -C blender
@@ -20,24 +25,3 @@ fi
 blender_path="${PWD}/${blender_unzipped_dir}/blender"
 echo "Blender downloaded to: ${blender_path}"
 
-echo "Updating editor settings"
-
-automation/linux/$godot_binary_name -v -e --quit --headless
-
-echo "EDITOR SETTINGS BEFORE ---------------------------------"
-cat ~/.config/godot/editor_settings-4.5.tres
-echo "-------------------------------------------------------"
-
-# Remove the old blender path
-sed -i '/.*rpc_port.*/d' ~/.config/godot/editor_settings-4.5.tres
-sed -i '/.*rpc_server_uptime.*/d' ~/.config/godot/editor_settings-4.5.tres
-sed -i '/.*blender_path.*/d' ~/.config/godot/editor_settings-4.5.tres
-
-# Add the blender path to the one we downloaded
-echo "filesystem/import/blender/blender_path = \"${blender_path}\"" >> ~/.config/godot/editor_settings-4.5.tres
-echo "filesystem/import/blender/rpc_port = 6011" >> ~/.config/godot/editor_settings-4.5.tres
-echo "filesystem/import/blender/rpc_server_uptime = 500" >> ~/.config/godot/editor_settings-4.5.tres
-
-echo "EDITOR SETTINGS AFTER ---------------------------------"
-cat ~/.config/godot/editor_settings-4.5.tres
-echo "-------------------------------------------------------"
