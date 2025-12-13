8ff9377901d031e184e4874eadbc7952528cb244
Daniel Savage <dansvg@gmail.com>
2025-12-14 07:01:55 +0900

    Title card sfx

--- hud/level_title_card.gd
+++ hud/level_title_card.gd
@@ -5,8 +5,12 @@ signal done()
 @onready var pre_title: Label = %PreTitle
 @onready var title: Label = %Title
 
+@onready var intro: AudioStreamPlayer = $AudioStreamPlayer
+
 func handle_flow() -> void:
 	var tree := get_tree()
+	
+	intro.play()
 	
 	var tween := tree.create_tween()
 	tween.tween_property(pre_title, "visible_ratio", 1.0, pre_title.text.length() * 0.0718)
@@ -14,7 +18,7 @@ func handle_flow() -> void:
 	tween.tween_property(title, "visible_ratio", 1.0, title.text.length() * 0.0818)
 	tween.tween_interval(2.0)
 	
-	await tween.finished
+	await intro.finished
 	
 	done.emit()
 
